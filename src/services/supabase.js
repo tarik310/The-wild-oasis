67d9a76b1f0f770da7cff723b84c75613d43c1d6
tarik310 <tareq310harh@gmail.com>
2024-04-09 10:13:12 +0300

    add starter files

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://mfmdsdspmappxqhmurok.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mbWRzZHNwbWFwcHhxaG11cm9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI1NzQ0MTAsImV4cCI6MjAyODE1MDQxMH0.Jrmu-STSZ6_Mrvr23ZkaxAdjvAWnO1n5RieJLtN0ytk";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
